466674026d9bc3b4eabe94343f07954245a91589
Maurice de Beijer <maurice.de.beijer@gmail.com>
2019-03-21 15:40:15 +0100

    Demo code

--- mod-5/src/calculator.js
+++ mod-5/src/calculator.js
@@ -1,3 +1,50 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var Logger = /** @class */ (function () {
     function Logger() {
     }
@@ -12,12 +59,16 @@ var Calculator = /** @class */ (function () {
     }
     Calculator.prototype.add = function (x, y) {
         var result = x + y;
-        this.logger.log(x + " + " + y + " = " + result);
+        if (this.logger) {
+            this.logger.log(x + " + " + y + " = " + result);
+        }
         return result;
     };
     Calculator.prototype.subtract = function (x, y) {
         var result = x - y;
-        this.logger.log(x + " - " + y + " = " + result);
+        if (this.logger) {
+            this.logger.log(x + " - " + y + " = " + result);
+        }
         return result;
     };
     return Calculator;
@@ -27,10 +78,129 @@ var y = document.getElementById('y');
 var add = document.getElementById('add');
 var subtract = document.getElementById('subtract');
 var result = document.getElementById('result');
-var calculator = new Calculator(new Logger());
-add.addEventListener('click', function () {
-    result.textContent = calculator.add(+x.value, +y.value).toString();
-});
+// const calculator = new Calculator(new Logger());
+var calculator = new Calculator(null);
+if (add && result) {
+    add.addEventListener('click', function () {
+        result.textContent = calculator.add(+x.value, +y.value).toString();
+    });
+}
 subtract.addEventListener('click', function () {
     result.textContent = calculator.subtract(+x.value, +y.value).toString();
 });
+var Dog = /** @class */ (function () {
+    function Dog() {
+    }
+    Dog.prototype.eat = function () {
+        return 1;
+    };
+    Dog.prototype.bark = function () { };
+    return Dog;
+}());
+var c1;
+var c2;
+var c3;
+c3 = {
+    bark: function () { },
+    eat: function () {
+        return 1;
+    },
+    sleep: function () {
+        return 1;
+    }
+};
+c3.bark();
+var Demo = /** @class */ (function () {
+    function Demo() {
+    }
+    Demo.prototype.sleep = function () {
+        return 1;
+    };
+    Demo.prototype.eat = function () {
+        return 1;
+    };
+    Demo.prototype.bark = function () { };
+    return Demo;
+}());
+var options = {
+    x: 1,
+    y: 2,
+    text: 'Doe maar'
+};
+function doeIets(o) { }
+function doeNogIets(option) {
+    var defaultOptions = {
+        x: 1,
+        y: 2,
+        text: 'Doe maar'
+    };
+    var actualOptopns = __assign({}, defaultOptions, options);
+}
+doeNogIets({});
+function test(param) {
+    if (param) {
+    }
+    else {
+    }
+}
+test(1);
+test(0);
+test(true);
+test(false);
+test('x');
+test('');
+// test('y');
+function getText(key) {
+    var teksts = {
+        yes: 'Ja',
+        no: 'Nee',
+        maybe: 'Misschien'
+    };
+    return teksts[key];
+}
+console.log(getText('yes'));
+getText('no');
+getText('maybe');
+var url = 'http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript';
+// function getData() {
+//   fetch(url)
+//     .then(rsp => rsp.json())
+//     .then(data => console.table(data.value))
+//     .catch(err => console.error(err));
+// }
+function sleep(ms) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(reject, ms);
+    });
+}
+function getData() {
+    return __awaiter(this, void 0, void 0, function () {
+        var rsp, data, e_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 5, , 6]);
+                    return [4 /*yield*/, fetch(url)];
+                case 1:
+                    rsp = _a.sent();
+                    if (!1) return [3 /*break*/, 4];
+                    return [4 /*yield*/, rsp.json()];
+                case 2:
+                    data = _a.sent();
+                    if (!data.value) return [3 /*break*/, 4];
+                    return [4 /*yield*/, sleep(5000)];
+                case 3:
+                    _a.sent();
+                    console.table(data.value);
+                    _a.label = 4;
+                case 4: return [3 /*break*/, 6];
+                case 5:
+                    e_1 = _a.sent();
+                    console.error(e_1);
+                    return [3 /*break*/, 6];
+                case 6: return [2 /*return*/];
+            }
+        });
+    });
+}
+getData();
--- mod-5/src/calculator.ts
+++ mod-5/src/calculator.ts
@@ -5,19 +5,22 @@ class Logger {
 }
 
 class Calculator {
-  constructor(private readonly logger: Logger) {}
+  constructor(private readonly logger: Logger | null) {}
 
-  add(x, y) {
+  add(x: number, y: number) {
     const result = x + y;
-    this.logger.log(`${x} + ${y} = ${result}`);
+    if (this.logger) {
+      this.logger.log(`${x} + ${y} = ${result}`);
+    }
 
     return result;
   }
 
-  subtract(x, y) {
+  subtract(x: number, y: number) {
     const result = x - y;
-    this.logger.log(`${x} - ${y} = ${result}`);
-
+    if (this.logger) {
+      this.logger.log(`${x} - ${y} = ${result}`);
+    }
     return result;
   }
 }
@@ -28,12 +31,144 @@ const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const result = document.getElementById('result');
 
-const calculator = new Calculator(new Logger());
+// const calculator = new Calculator(new Logger());
+const calculator = new Calculator(null);
 
-add.addEventListener('click', () => {
-  result.textContent = calculator.add(+x.value, +y.value).toString();
+if (add && result) {
+  add.addEventListener('click', () => {
+    result.textContent = calculator.add(+x.value, +y.value).toString();
+  });
+}
+
+subtract!.addEventListener('click', () => {
+  result!.textContent = calculator.subtract(+x.value, +y.value).toString();
 });
 
-subtract.addEventListener('click', () => {
-  result.textContent = calculator.subtract(+x.value, +y.value).toString();
-});
+class Dog {
+  eat() {
+    return 1;
+  }
+  bark() {}
+}
+
+interface Cat {
+  eat(): string;
+  sleep(): void | number;
+}
+
+type X = Cat | Dog;
+type Y = Cat & Dog;
+
+let c1: Cat;
+let c2: X;
+let c3: Y;
+
+c3 = {
+  bark() {},
+  eat() {
+    return 1 as any;
+  },
+  sleep() {
+    return 1;
+  }
+};
+
+c3.bark();
+
+interface Katje {
+  sleep(): number;
+}
+
+class Demo implements Y {
+  sleep() {
+    return 1;
+  }
+  eat() {
+    return 1 as any;
+  }
+  bark() {}
+}
+
+const options = {
+  x: 1,
+  y: 2,
+  text: 'Doe maar'
+};
+
+type Options = typeof options;
+
+function doeIets(o: Options) {}
+
+function doeNogIets(option: Partial<typeof defaultOptions>) {
+  const defaultOptions = {
+    x: 1,
+    y: 2,
+    text: 'Doe maar'
+  };
+
+  const actualOptopns = { ...defaultOptions, ...options };
+}
+
+doeNogIets({});
+
+function test(param: boolean | 0 | 1 | '' | 'x') {
+  if (param) {
+  } else {
+  }
+}
+
+test(1);
+test(0);
+test(true);
+test(false);
+test('x');
+test('');
+// test('y');
+
+function getText(key: keyof typeof teksts) {
+  const teksts = {
+    yes: 'Ja',
+    no: 'Nee',
+    maybe: 'Misschien'
+  };
+
+  return teksts[key];
+}
+
+console.log(getText('yes'));
+getText('no');
+getText('maybe');
+
+const url =
+  'http://api.icndb.com/jokes/random/10/?limitTo=[nerdy]&escape=javascript';
+
+// function getData() {
+//   fetch(url)
+//     .then(rsp => rsp.json())
+//     .then(data => console.table(data.value))
+//     .catch(err => console.error(err));
+// }
+
+function sleep(ms: number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(reject, ms);
+  });
+}
+
+async function getData() {
+  try {
+    const rsp = await fetch(url);
+    if (1) {
+      const data = await rsp.json();
+      if (data.value) {
+        await sleep(5000);
+
+        console.table(data.value);
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+getData();
